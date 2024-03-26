0b5d8b67123265cccfb84106fee571cad2ace569
Sachin Kushwaha <sachinkeshav@gmail.com>
2024-03-26 00:18:00 -0700

    @NonNull is meaningless on a primitive

--- src/main/java/com/skushwaha/synchrony/project/response/Response.java
+++ src/main/java/com/skushwaha/synchrony/project/response/Response.java
@@ -4,14 +4,13 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import java.io.Serializable;
 import lombok.Getter;
-import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 
 @Getter
 @RequiredArgsConstructor
 @JsonInclude(Include.NON_EMPTY)
 public class Response<T> implements Serializable {
-  private final @NonNull int status;
-  private final @NonNull boolean success;
-  private final @NonNull T data;
+  private final int status;
+  private final boolean success;
+  private final T data;
 }
